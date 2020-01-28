93865b2987b710391ba84b66cee6603f630552af
Craig H <ch40@hw.ac.uk>
2020-01-28 00:14:17 +0000

    Added Cluster to local file

--- README.md
+++ README.md
@@ -24,10 +24,11 @@ ROS/Gazebo simulation packages for driverless FSAE vehicles.
 * ros-kinetic-velocity-controllers
 * ros-kinetic-joint-state-controller
 * ros-kinetic-gazebo-ros-control
+* ros-kinetic-perception-pcl
 
 Or if you are lazy like my here's a one-liner
 ```
-sudo apt-get install ros-kinetic-ackermann-msgs ros-kinetic-twist-mux ros-kinetic-joy ros-kinetic-controller-manager ros-kinetic-robotnik-msgs ros-kinetic-velodyne-simulator ros-kinetic-effort-controllers ros-kinetic-velocity-controllers ros-kinetic-joint-state-controller ros-kinetic-gazebo-ros-control ros-kinetic-robotnik-msgs
+sudo apt-get install ros-kinetic-ackermann-msgs ros-kinetic-twist-mux ros-kinetic-joy ros-kinetic-controller-manager ros-kinetic-robotnik-msgs ros-kinetic-velodyne-simulator ros-kinetic-effort-controllers ros-kinetic-velocity-controllers ros-kinetic-joint-state-controller ros-kinetic-gazebo-ros-control ros-kinetic-robotnik-msgs ros-kinetic-perception-pcl
 ```
 
 
--- lidar/src/lidar_perception.cpp
+++ lidar/src/lidar_perception.cpp
@@ -12,50 +12,68 @@
 #include <limits>
 #include <pcl/common/common.h>
 
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <visualization_msgs/Marker.h>
+
+
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 typedef pcl::PointXYZ Point;
+
 #define LOWERBOUNDS 0
 #define UPPERBOUNDS 5
 #define GPRDELTA 0.15
 ros::Publisher pub;
 
 
-
 void PCLcallback(const sensor_msgs::PointCloud2ConstPtr& input){
-    //"Size Pre filter"
-    ROS_INFO("POINTCLOUD RECIEVED");
-    ROS_INFO("Height %d", input->height);
-    ROS_INFO("Width %d", input->width);
 
+    //Input Cloud
+    ROS_INFO("POINTCLOUD RECIEVED");
+    // ROS_INFO("Height %d", input->height);
+    // ROS_INFO("Width %d", input->width);
+
+	//Convert to usable form
     pcl::PCLPointCloud2 pcl_pc2; 
     PointCloud::Ptr pre(new PointCloud);
     pcl_conversions::toPCL(*input, pcl_pc2);    //Converts from ROS Point Cloud 2 to PCL Point Cloud 2
     pcl::fromPCLPointCloud2(pcl_pc2,*pre);      //Converts from PCL Point Cloud 2 to PCL Point Cloud
 
+	//Begin FOV Trimming
     PointCloud::Ptr post(new PointCloud);
     pcl::PassThrough<pcl::PointXYZ> pass;       //Create Filter
     pass.setInputCloud(pre);
     pass.setFilterFieldName("x");
     pass.setFilterLimits(LOWERBOUNDS,UPPERBOUNDS);
-    pass.filter(*post);                         //Filter pre Cloud based on Above parameters
+    pass.filter(*post);                         //Filter pre Cloud based on Above parameters ()
 
-    //"Size" Post Filter
-    ROS_INFO("POINTCLOUD Filtered");    
-    ROS_INFO("Height %d", post->height);
-    ROS_INFO("Width %d", post->width);
+    // ROS_INFO("POINTCLOUD Filtered");    
+    // ROS_INFO("Height %d", post->height);
+    // ROS_INFO("Width %d", post->width);
+    //Finished FOV Trimming
 
-    Eigen::Vector4f min, max;
+	//Begins Ground Plane Removal
+    Eigen::Vector4f min, max; //Vectors to hold max and min points
     pcl::getMinMax3D(*post,min,max);
 
-    ROS_INFO("MIN %d", min);
-    ROS_INFO("Lowest Point X %f", min[0]);
-    ROS_INFO("Lowest Point Y %f", min[1]);
-    ROS_INFO("Lowest Point Z %f", min[2]);
+    // ROS_INFO("MIN %d", min);
+    // ROS_INFO("Lowest Point X %f", min[0]);
+    // ROS_INFO("Lowest Point Y %f", min[1]);
+    // ROS_INFO("Lowest Point Z %f", min[2]);
 
-    ROS_INFO("MAX %d", max);
-    ROS_INFO("Highest Point X %f", max[0]);
-    ROS_INFO("Highest Point Y %f", max[1]);
-    ROS_INFO("Highest Point Z %f", max[2]);
+    // ROS_INFO("MAX %d", max);
+    // ROS_INFO("Highest Point X %f", max[0]);
+    // ROS_INFO("Highest Point Y %f", max[1]);
+    // ROS_INFO("Highest Point Z %f", max[2]);
 
     PointCloud::Ptr GPR(new PointCloud);
     pcl::PassThrough<pcl::PointXYZ> gprpass;       //Create Filter
@@ -64,13 +82,48 @@ void PCLcallback(const sensor_msgs::PointCloud2ConstPtr& input){
     gprpass.setFilterLimitsNegative(true);
     gprpass.setFilterLimits(min[2],min[2]+GPRDELTA);
     gprpass.filter(*GPR);                         //Filter pre Cloud based on Above parameters
+    //Finished Ground Plane Removal
+
+	//Begin Clustering
+    pcl::search::KdTree<Point>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud(GPR);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+
+	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+	ec.setClusterTolerance(0.3); // 10cm
+	ec.setMinClusterSize(3);
+	ec.setMaxClusterSize(40);
+	ec.setSearchMethod(tree);
+	ec.setInputCloud(GPR);
+	ec.extract(cluster_indices);
+	
+	pcl::PCDWriter writer;
+	int j = 0;
+	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+	{
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+		for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+		cloud_cluster->points.push_back (GPR->points[*pit]); //*
+		cloud_cluster->width = cloud_cluster->points.size ();
+		cloud_cluster->height = 1;
+		cloud_cluster->is_dense = true;
+
+		std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+		std::stringstream ss;
+		ss << "cloud_cluster_" << j << ".pcd";
+		writer.write<pcl::PointXYZ> (ss.str (), *cloud_cluster, false); //*
+		j++;
+	}
 
 
+	//Convert Back to ROS usable format
     pcl::PCLPointCloud2 post_pc2;
     sensor_msgs::PointCloud2 output;
     pcl::toPCLPointCloud2(*GPR,post_pc2);      //Convert from PCL Point Cloud to PCL Point Cloud 2
     pcl_conversions::fromPCL(post_pc2,output);  //Convert from PCL Point Cloud 2 to ROS Point Cloud 2
     
+	//Published Filtered Cloud
     pub.publish(output);
 
 }
