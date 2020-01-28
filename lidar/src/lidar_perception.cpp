467596a8184fe88c2734ebecc43735efe6a57068
Craig <ch40@hw.ac.uk>
2020-01-28 18:39:27 +0000

    Now generates marker as a bounding box around the area that will be restored

--- lidar/src/lidar_perception.cpp
+++ lidar/src/lidar_perception.cpp
@@ -1,5 +1,6 @@
 //C Includes
 #include <limits>
+#include <string>
 //ROS Inlcudes
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -36,101 +37,175 @@ typedef pcl::PointXYZ Point;
 #define UPPERBOUNDS 5
 //Ground Plane Removal Parameters
 #define GPRDELTA 0.15
+
+//Bounding Box Scalers
+#define BOUNDINGBOXSIZESCALER 3
+#define BOUNDINGBOXHEIGHTSCALER 1
+#define MINIMUMBOXSIZE 0.08
 //Debug Parameters
 //#define LOGTOFILE 1
 
 //Global publisher variable
 ros::Publisher pub;
+ros::Publisher markers;
+
+//Filter Method
+void passThroughFilter(PointCloud::Ptr inputCloud, std::string filterField, float lowerLimit, float upperLimit, PointCloud::Ptr output, bool negative){
+    pcl::PassThrough<pcl::PointXYZ> pass;       //Create Filter
+    pass.setInputCloud(inputCloud);
+    pass.setFilterFieldName(filterField);
+    pass.setFilterLimits(lowerLimit,upperLimit);
+    if(negative){
+        pass.setFilterLimitsNegative(true);
+    }
+    pass.filter(*output);                         //Filter pre Cloud based on Above parameters ()
+
+}
+
+//Convert Down Method
+void ConvertToPCL(sensor_msgs::PointCloud2ConstPtr input, PointCloud::Ptr output){
+
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*input, pcl_pc2);    //Converts from ROS Point Cloud 2 to PCL Point Cloud 2
+    pcl::fromPCLPointCloud2(pcl_pc2,*output);      //Converts from PCL Point Cloud 2 to PCL Point Cloud
+
+}
+
+//Convert Up Method
+sensor_msgs::PointCloud2 ConvertFromPCL(PointCloud::Ptr  input){
+
+    sensor_msgs::PointCloud2 output;
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl::toPCLPointCloud2(*input,pcl_pc2);      //Convert from PCL Point Cloud to PCL Point Cloud 2
+    pcl_conversions::fromPCL(pcl_pc2,output);  //Convert from PCL Point Cloud 2 to ROS Point Cloud 2
+
+    return output;
+}
+
+//Bounding Box Method
+void getBoundingBox(PointCloud::Ptr inputCloud,geometry_msgs::Pose* pose, geometry_msgs::Vector3* dimensions){
+
+    Eigen::Vector4f min, max; //Vectors to hold max and min points
+    pcl::getMinMax3D(*inputCloud,min,max); //Get max and min points
+    
+    //Determine the Position of the box based on the mid point of the min and max value
+    pose->position.x = (max.x() + min.x())/2;
+    pose->position.y = (max.y() + min.y())/2;
+    pose->position.z = (max.z() + min.z())/2;
+
+    //Determine the larger size of the bounding box 
+    float xDiff = max.x() - min.x();
+    float yDiff = max.y() - min.y();
+    float xySize = 0;
+
+    if (xDiff>yDiff){
+        xySize = xDiff;
+    }else{
+        xySize = yDiff;
+    }
+
+    if(xySize< MINIMUMBOXSIZE){
+        xySize = MINIMUMBOXSIZE;
+    }
+
+    //Assign bounding box size to message 
+    dimensions->x =  xySize*BOUNDINGBOXSIZESCALER;
+    dimensions->y = xySize*BOUNDINGBOXSIZESCALER;
+    dimensions->z = BOUNDINGBOXHEIGHTSCALER;
+
+    ROS_INFO("BOX X %f", dimensions->x);
+    ROS_INFO("BOX Y %f", dimensions->y);
+    ROS_INFO("BOX Z %f", dimensions->z);
+
+}
+//Clustering Method
 
 void PCLcallback(const sensor_msgs::PointCloud2ConstPtr& input){
 
     //Input Cloud
     ROS_INFO("POINTCLOUD RECIEVED");
-    // ROS_INFO("Height %d", input->height);
-    // ROS_INFO("Width %d", input->width);
+/*     ROS_INFO("Height %d", input->height);
+    ROS_INFO("Width %d", input->width); */
 
 	//Convert to usable form
-    pcl::PCLPointCloud2 pcl_pc2;
     PointCloud::Ptr pre(new PointCloud);
-    pcl_conversions::toPCL(*input, pcl_pc2);    //Converts from ROS Point Cloud 2 to PCL Point Cloud 2
-    pcl::fromPCLPointCloud2(pcl_pc2,*pre);      //Converts from PCL Point Cloud 2 to PCL Point Cloud
+    ConvertToPCL(input,pre);
 
 	//Begin FOV Trimming
     PointCloud::Ptr post(new PointCloud);
-    pcl::PassThrough<pcl::PointXYZ> pass;       //Create Filter
-    pass.setInputCloud(pre);
-    pass.setFilterFieldName("x");
-    pass.setFilterLimits(LOWERBOUNDS,UPPERBOUNDS);
-    pass.filter(*post);                         //Filter pre Cloud based on Above parameters ()
+    passThroughFilter(pre, "x", LOWERBOUNDS, UPPERBOUNDS, post, false);
 
-    // ROS_INFO("POINTCLOUD Filtered");    
-    // ROS_INFO("Height %d", post->height);
-    // ROS_INFO("Width %d", post->width);
+/*     ROS_INFO("POINTCLOUD Filtered");    
+    ROS_INFO("Height %d", post->height);
+    ROS_INFO("Width %d", post->width); */
     //Finished FOV Trimming
 
 	//Begins Ground Plane Removal
     Eigen::Vector4f min, max; //Vectors to hold max and min points
     pcl::getMinMax3D(*post,min,max);
 
-    // ROS_INFO("MIN %d", min);
-    // ROS_INFO("Lowest Point X %f", min[0]);
-    // ROS_INFO("Lowest Point Y %f", min[1]);
-    // ROS_INFO("Lowest Point Z %f", min[2]);
+/*     ROS_INFO("MIN %d", min);
+    ROS_INFO("Lowest Point X %f", min[0]);
+    ROS_INFO("Lowest Point Y %f", min[1]);
+    ROS_INFO("Lowest Point Z %f", min[2]);
 
-    // ROS_INFO("MAX %d", max);
-    // ROS_INFO("Highest Point X %f", max[0]);
-    // ROS_INFO("Highest Point Y %f", max[1]);
-    // ROS_INFO("Highest Point Z %f", max[2]);
+    ROS_INFO("MAX %d", max);
+    ROS_INFO("Highest Point X %f", max[0]);
+    ROS_INFO("Highest Point Y %f", max[1]);
+    ROS_INFO("Highest Point Z %f", max[2]); */
 
-    PointCloud::Ptr GPR(new PointCloud);
-    pcl::PassThrough<pcl::PointXYZ> gprpass;       //Create Filter
-    gprpass.setInputCloud(post);
-    gprpass.setFilterFieldName("z");
-    gprpass.setFilterLimitsNegative(true);
-    gprpass.setFilterLimits(min[2],min[2]+GPRDELTA);
-    gprpass.filter(*GPR);                         //Filter pre Cloud based on Above parameters
+    PointCloud::Ptr gpr(new PointCloud);
+    passThroughFilter(post, "z", min[2],min[2]+GPRDELTA, gpr, true);
     //Finished Ground Plane Removal
 
 	//Begin Clustering
     pcl::search::KdTree<Point>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
-    tree->setInputCloud(GPR);
+    tree->setInputCloud(gpr);
 
     std::vector<pcl::PointIndices> cluster_indices;
 
 	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-	ec.setClusterTolerance(0.3); // 10cm
-	ec.setMinClusterSize(3);
-	ec.setMaxClusterSize(40);
+	ec.setClusterTolerance(0.3); // 30cm
+	ec.setMinClusterSize(2);
+	ec.setMaxClusterSize(20);
 	ec.setSearchMethod(tree);
-	ec.setInputCloud(GPR);
+	ec.setInputCloud(gpr);
 	ec.extract(cluster_indices);
-	
-    #ifdef LOGTOFILE
-	pcl::PCDWriter writer;
+
 	int j = 0;
 	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
 	{
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+		PointCloud::Ptr cloud_cluster (new PointCloud);
 		for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
-		cloud_cluster->points.push_back (GPR->points[*pit]); //*
-		cloud_cluster->width = cloud_cluster->points.size ();
+		cloud_cluster->points.push_back(gpr->points[*pit]); //*
+		cloud_cluster->width = cloud_cluster->points.size();
 		cloud_cluster->height = 1;
 		cloud_cluster->is_dense = true;
 
-		std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+		std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size() << " data points." << std::endl;
 		std::stringstream ss;
-		ss << "cloud_cluster_" << j << ".pcd";
-		writer.write<pcl::PointXYZ> (ss.str (), *cloud_cluster, false); //*
+		ss << "cloud_cluster_" << j << ".pcd"; 
+
+        //Create Marker Box for Cone
+        visualization_msgs::Marker object_marker;
+        object_marker.ns = "cluster";
+        object_marker.id = j;
+        object_marker.header.frame_id="velodyne";
+        object_marker.type = visualization_msgs::Marker::CUBE;
+        getBoundingBox(cloud_cluster, &object_marker.pose, &object_marker.scale);
+        object_marker.color.g=1;
+        object_marker.color.a=0.3;
+
+        //CropBox
+
+        markers.publish(object_marker);
+
 		j++;
 	}
-    #endif
-
 
 	//Convert Back to ROS usable format
-    pcl::PCLPointCloud2 post_pc2;
     sensor_msgs::PointCloud2 output;
-    pcl::toPCLPointCloud2(*GPR,post_pc2);      //Convert from PCL Point Cloud to PCL Point Cloud 2
-    pcl_conversions::fromPCL(post_pc2,output);  //Convert from PCL Point Cloud 2 to ROS Point Cloud 2
+    output = ConvertFromPCL(gpr);
     
 	//Published Filtered Cloud
     pub.publish(output);
@@ -146,6 +221,7 @@ int main(int argc, char **argv){
 
     ros::Subscriber sub = n.subscribe("velodyne_points", 1, PCLcallback); //Create Subscriber to velodyne 
     pub = n.advertise<PointCloud> ("FilterPoints",1); //Create Publisher for filtered cloud
+    markers = n.advertise<visualization_msgs::Marker>("ConeMarkers",1);
     ros::spin();
 
     return(0);
