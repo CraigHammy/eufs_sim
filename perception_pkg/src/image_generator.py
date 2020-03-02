0eaf32f7584f0ce462ff8127117daa6aaca2b434
Craig H <ch40@hw.ac.uk>
2020-03-02 10:51:19 +0000

    Ok this time

---
+++ perception_pkg/src/image_generator.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+
+#Taken from https://gist.github.com/rethink-imcmahon/77a1a4d5506258f3dc1f#file-ros_image_saver-py and modified for our use
+
+# rospy for the subscriber
+import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+import os
+
+# Instantiate CvBridge
+bridge = CvBridge()
+image_folder_path = '/home/craig/catkin_ws/src/eufs_sim/perception_pkg/camera_data/right_camera_saved'
+
+def image_callback(msg):
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg
+        i = len(os.listdir(image_folder_path))
+        print i 
+        image_name = 'rightcamera' + str(i) +'.jpg'
+        cv2.imwrite(image_name, cv2_img)
+        rospy.sleep(1)
+
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    image_topic = "/zed/right/image_rect_color"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, image_callback)
+
+    # Spin until ctrl + c
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
